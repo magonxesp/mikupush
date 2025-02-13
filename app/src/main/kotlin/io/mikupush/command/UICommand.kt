c4a95529140850dde86737360ba4119c8f1925c5
MagonxESP <janma.360@gmail.com>
2025-02-13 18:44:16 +0100

    feat: allow start only on system tray

--- app/src/main/kotlin/io/mikupush/command/UICommand.kt
+++ app/src/main/kotlin/io/mikupush/command/UICommand.kt
@@ -6,20 +6,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.application
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 import io.mikupush.http.startHttpServer
-import io.mikupush.notification.Notifier
 import io.mikupush.setupDatabase
 import io.mikupush.ui.tray.AppTrayIcon
 import io.mikupush.ui.window.MainWindow
-import org.koin.java.KoinJavaComponent.inject
 import org.slf4j.LoggerFactory
 
 class UICommand : CliktCommand(name = "ui") {
     private val logger = LoggerFactory.getLogger(this::class.java)
-    private val notifier: Notifier by inject(Notifier::class.java)
+    private val onlyTray by option("-t", "--tray").flag(default = false)
 
     private fun launchUI() = application {
-        var openUploadsWindow by rememberSaveable { mutableStateOf(true) }
+        var openUploadsWindow by rememberSaveable { mutableStateOf(!onlyTray) }
 
         AppTrayIcon(onOpen = { openUploadsWindow = true })
         MainWindow(openUploadsWindow, onCloseRequest = { openUploadsWindow = false })
